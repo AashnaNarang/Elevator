7b773e38199f7137b74a6ae0457eb24832fc5439
danihashweh <55706559+danihashweh@users.noreply.github.com>
2021-02-01 15:55:15 -0500

    Created Floor helper classes.
    - Created direction.java, floorbutton.java, floorlamp.java

---
+++ src/FloorButton.java
@@ -0,0 +1,14 @@
+
+public class FloorButton {
+	private Direction direction; 
+	private FloorLamp lamp; 
+	
+	public FloorButton(Direction direction, FloorLamp lamp) {
+		this.direction = direction; 
+		this.lamp = lamp; 
+	}
+	
+	public void click() {
+		
+	}
+}
