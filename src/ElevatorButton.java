e063dccc752505334454c24f2c7142d848bf493b
Itzaek <isaac1937@gmail.com>
2021-02-03 23:01:46 -0500

    Added new classes, started a little bit of elevator implementation

--- src/ElevatorButton.java
+++ src/ElevatorButton.java
@@ -1,3 +1,16 @@
 public class ElevatorButton {
+  FloorLamp lamp;
 
+  public ElevatorButton() {
+    lamp = new FloorLamp(false);
+  }
+
+  public void click() {
+    if(lamp.isOn) {
+      lamp.setOn(false);
+    }
+    else {
+      lamp.setOn(true);
+    }
+  }
 }
