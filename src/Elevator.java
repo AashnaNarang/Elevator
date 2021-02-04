ead88f160a6cc6daddf42e80452be1cd5df94065
yuvrajzala26 <78375636+yuvrajzala26@users.noreply.github.com>
2021-02-03 22:12:22 -0700

    Just A basic idead that I have implemeted Still need help and work on

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,41 +1,64 @@
 import java.lang.Math;
 import java.time.LocalTime;
+import java.util.ArrayList;
+
+/*
+ * The Elevator class is designed so that it takes the task form the middleman
+ * and moves the elevator up/down based on the given task it will receive.
+ */
 
 public class Elevator implements Runnable {
   private int currentFloor;
-  private boolean isStationary;
-  private Middleman middleman;
+  int numFloor;
+  private MiddleMan middleMan;
   private Direction direction;
   private ArrayList<ElevatorButton> buttons;
 
-  public Elevator(Middleman middleman) {
-    this.middleman = middleman;
-    isStationary = true;
+  /* constructor for Elevator
+   * Defining the middleclass parameters that are by to the scheduler.
+   *
+   * @param middleman - sending information to Middleman
+   */
+  public Elevator(MiddleMan middleMan, int numFloor) {
+    this.middleMan = middleMan;
     currentFloor = 0;
     direction = Direction.UP;
     buttons = new ArrayList<ElevatorButton>();
 
-    //Create an array of elevator buttons
-    for(int i = 0;i < 10;i++) {
+    for (int i = 0; i < numFloor; i++) {
       buttons.add(new ElevatorButton());
     }
   }
 
-  /*public void clickButton(int button) {
-    buttons.get(button).click();
-
-    //TODO Time, how long?
-    scheduler.putEvent(new ElevatorEvent(this.currentFloor, button, new Time(), this);
-  }*/
-
+  /*
+   * This run method will set the information to the middleman as we try to
+   * update the middleman will the infomation This methond will also update the
+   * current floor elevator is moving through.
+   */
   public void run() {
-    FloorEvent floorevent = middleman.getFloorEvent();
-    int floors = floorevent.getDestination() - floorevent.getSource();
-    currentFloor += floors;
+    while (true) {
+      FloorEvent floorEvent = middleMan.getFloorEvent();
+      if (floorEvent != null) {
+        buttons.get(floorEvent.getDestination()).click();
 
-    ArrivalEvent arrivalEvent = new ArrivalEvent(this.currentFloor, LocalTime.now(), this);
-    middleman.putArrivalEvent();
+        boolean directionUp = true;
+        int floors = floorEvent.getDestination() - floorEvent.getSource();
+        if (floors < 0) {
+          directionUp = false;
+        }
 
-
+        for (int i = 0; i < Math.abs(floors); i++) {
+          if (directionUp) {
+            currentFloor++;
+          } else {
+            currentFloor--;
+          }
+          ArrivalEvent arrivalEvent =
+              new ArrivalEvent(this.currentFloor, LocalTime.now(), this);
+          middleMan.putArrivalEvent(arrivalEvent);
+        }
+      }
+      buttons.get(this.currentFloor).click();
+    }
   }
 }
