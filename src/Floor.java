fe6347a64ca8fe898a328991d75d62eca0782735
danihashweh <55706559+danihashweh@users.noreply.github.com>
2021-02-05 17:28:47 -0500

    Implemented the floor and lamp.
    Implemented the classes Floor.java, FloorButton.java, and Lamp.java.
    
    Deleted FloorLamp since its not needed anymore.

--- src/Floor.java
+++ src/Floor.java
@@ -1,19 +1,40 @@
-
+/**
+ * The floor class is responsible for tracking the floor number and also toggling the lights for the up/down buttons. 
+ * @author danih
+ *
+ */
 public class Floor {
-	
-
 	private int floorNumber;
 	private FloorButton upButton;
 	private FloorButton downButton;
 	
+	/**
+	 * Constructor for Floor. 
+	 * @param floorNumber - The current floor of the elevator. 
+	 */
 	public Floor(int floorNumber) {
 		this.floorNumber = floorNumber;
 	}
 	
-	public void click() {
-		
+	/**
+	 * Turns on the Lamp depending on the direction of the elevator. 
+	 * If the elevator is going up, then the lamp will turn on the up lamp. 
+	 * @param direction
+	 * @param lampStatus
+	 */
+	public void turnButtonOn(Direction direction, boolean lampStatus) {
+		if(direction == Direction.UP){
+			upButton.switchOn(lampStatus);
+		}
+		else {
+			downButton.switchOn(lampStatus);
+		}
 	}
 	
+	/**
+	 * Getter method for the floor number
+	 * @return the current floor number of the elevator. 
+	 */
 	public int getFloorNumber() {
 		return floorNumber;
 	}
--- src/FloorButton.java
+++ src/FloorButton.java
@@ -1,14 +1,26 @@
-
+/**
+ * FloorButton class which contains the direction that the elevator is going and the lamp status. 
+ */
 public class FloorButton {
 	private Direction direction; 
-	private FloorLamp lamp; 
+	private Lamp lamp; 
 	
-	public FloorButton(Direction direction, FloorLamp lamp) {
+	/**
+	 * Constructor for FloorButton
+	 * 
+	 * @param direction - The direction of the elevator (UP/DOWN)
+	 * @param lamp - Contains the status of the lamp for the elevator. 
+	 */
+	public FloorButton(Direction direction, Lamp lamp) {
 		this.direction = direction; 
 		this.lamp = lamp; 
 	}
 	
-	public void click() {
-		
+	/**
+	 * Turns the lamp on/off based on the lamp status. 
+	 * @param lampStatus - boolean which is true if the lamp is on, and off if false. 
+	 */
+	public void switchOn(boolean lampStatus) {
+		lamp.switchLampStatus(lampStatus);
 	}
 }
