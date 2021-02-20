e4086d7d47fd52e3c8dcdb3756bbeb97dfe3c855
danihashweh <55706559+danihashweh@users.noreply.github.com>
2021-02-20 13:35:57 -0500

    Create the schedulersubsystem and the active state for iteration 2

---
+++ src/ActiveState.java
@@ -0,0 +1,17 @@
+
+public class ActiveState {
+	//will implement the state interface
+	private SchedulerSubsystem schedulerSubsystem; 
+	
+	public ActiveState(Scheduler scheduler) {
+		
+	}
+	
+	public void handleFloorEvent() {
+		
+	}
+	
+	public void sendArrivalEvent() {
+		
+	}
+}
---
+++ src/SchedulerSubsystem.java
@@ -0,0 +1,19 @@
+
+public class SchedulerSubsystem implements Runnable{
+	
+	public SchedulerSubsystem() {
+		
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void handleFloorEvent() {
+		
+	}
+	
+	public void sendArrivalEvent() {
+		
+	}
+}
