d91139e13e540d30abfed06da006028b0a30fbbc
Abdoltim <a.elnakla@hotmail.com>
2021-03-09 23:08:37 -0500

    Updated some tests
    Updated parsevent test

--- src/test/IterationOneTest.java
+++ src/test/IterationOneTest.java
@@ -2,11 +2,14 @@ package test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalTime;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import events.FloorEvent;
+import main.Direction;
 import main.Elevator;
 import main.FloorSubsystem;
 import main.MiddleMan;
@@ -43,5 +46,23 @@ public class IterationOneTest {
 		} catch (InterruptedException e) {
 		}
 	}
-
+	
+	@Test
+	public void parseEvents() {
+			//08:10:23.100 1 up 3
+		try {
+			floorSubsystemThread = new Thread(new FloorSubsystem("input.txt", 4, middleMan1), "floorSubsystem");
+			floorSubsystemThread.start();
+			TimeUnit.SECONDS.sleep(1);
+			FloorEvent floorEvent = middleMan1.getFloorEvent();
+			assertEquals(floorEvent.getSource(), 1);
+			assertEquals(floorEvent.getDestination(), 3);
+			assertEquals(floorEvent.getDirection(), Direction.UP);
+			assertEquals(floorEvent.getTime(), LocalTime.parse("08:10:23.100"));
+			}catch (InterruptedException e) {
+			}
+	}
+	
 }
+
+
