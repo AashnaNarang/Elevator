af3777728fa34dee65c5d5031d43b2161324585e
krish <krishaneasparan@cmail.carleton.ca>
2021-03-27 16:30:19 -0400

    added 4 elevators to main.

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		Thread floorSubsystem = new Thread(new FloorSubsystem("input9.txt", Configurations.FLOOR_PORT, Configurations.FLOOR_EVENT_PORT), "floorSubsystem");
+		Thread floorSubsystem = new Thread(new FloorSubsystem("input7.txt", Configurations.FLOOR_PORT, Configurations.FLOOR_EVENT_PORT), "floorSubsystem");
 		Thread sched = new Thread(new Scheduler(Configurations.FLOOR_EVENT_PORT
 				, Configurations.ARRIVAL_PORT, Configurations.DEST_PORT, Configurations.FLOOR_PORT, Configurations.ELEVATOR_STAT_PORT, Configurations.TIMER_PORT), "scheduler");
 		Thread elevator0 = new Thread(new Elevator(Configurations.ELEVATOR_FLOOR_PORT, Configurations.ELEVATOR_SCHEDULAR_PORT, 
@@ -11,10 +11,18 @@ public class Main {
 		Thread elevator1 = new Thread(new Elevator(Configurations.ELEVATOR_FLOOR_PORT + 1, 
 				Configurations.ELEVATOR_SCHEDULAR_PORT + 1,
 				Configurations.ARRIVAL_PORT, Configurations.DEST_PORT, Configurations.ELEVATOR_STAT_PORT), "elevator1");
+		Thread elevator2 = new Thread(new Elevator(Configurations.ELEVATOR_FLOOR_PORT + 2, 
+				Configurations.ELEVATOR_SCHEDULAR_PORT + 2,
+				Configurations.ARRIVAL_PORT, Configurations.DEST_PORT, Configurations.ELEVATOR_STAT_PORT), "elevator2");
+		Thread elevator3 = new Thread(new Elevator(Configurations.ELEVATOR_FLOOR_PORT + 3, 
+				Configurations.ELEVATOR_SCHEDULAR_PORT + 3,
+				Configurations.ARRIVAL_PORT, Configurations.DEST_PORT, Configurations.ELEVATOR_STAT_PORT), "elevator3");
 		floorSubsystem.start();
 		sched.start();
 		elevator0.start();
 		elevator1.start();
+		elevator2.start();
+		elevator3.start();
 	}
 
 }
