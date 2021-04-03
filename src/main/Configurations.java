4b84a760aac68155f464469ab0716a978052fe2e
yuvrajzala26 <78375636+yuvrajzala26@users.noreply.github.com>
2021-04-03 16:11:11 -0600

    updated the config and elevator for time during moving thru floors and load and unload when at rest.

--- src/main/Configurations.java
+++ src/main/Configurations.java
@@ -38,6 +38,12 @@ public final class Configurations {
 	// Amount of time that should occur between receiving a packet and sending
 	// another
 	// This can be set to 1000 if the code should run quickly
-	public static final int SLEEP_DURATION_MILLIS = 5000;
+	public static final int SLEEP_DURATION_MILLIS = 1000;
+	
+	public static final int TIME_MOVING_BETWEEN_FLOOR = 1000;
+	
+	public static final int TIME_TO_LOAD_UNLOAD = 5000;
+	
+	
 
 }
